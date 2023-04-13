02c2741b7aa89d417282378c073923f77b63a69d
MarecoAJ <alejo_mareco@hotmail.com>
2023-04-13 18:47:18 -0300

    funcionalidad mobile. retoques de menus

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 navEmail.addEventListener("click", togggleDesktopMenu);
+menuIcon.addEventListener("click", toggleMobileMenu);
+
 function  togggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function  toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
