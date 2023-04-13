47372cab56f197b6386d446e0320708ff7817147
MarecoAJ <alejo_mareco@hotmail.com>
2023-04-13 19:44:50 -0300

    ensamble y funcionalidad de carrito. optimizacion de colicion

--- main.js
+++ main.js
@@ -2,14 +2,42 @@ const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
 navEmail.addEventListener("click", togggleDesktopMenu);
 menuIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
-function  togggleDesktopMenu(){
+function togggleDesktopMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+
     desktopMenu.classList.toggle("inactive");
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
 }
 
-function  toggleMobileMenu(){
+function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains("inactive");
+
     mobileMenu.classList.toggle("inactive");
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+    const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+
+    aside.classList.toggle("inactive");
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add("inactive");
+    }
 }
